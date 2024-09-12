5fcca347cea4b77edaf2c6195cf602ddb914f691
Jeferson santos <jefersonrouue@gmail.com>
2024-09-12 17:11:24 -0300

    Vercel file

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ console.log(sequelize instanceof Sequelize);
 
 
 //rodando server
-const port = 3001;
-app.listen(port, () => {
+const PORT = process.env.PORT || 3000; 3001;
+app.listen(PORT, () => {
     console.log('rodando url http://localhost3001')
 })
